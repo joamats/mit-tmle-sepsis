92d2be9d50ea76909d38244959a69d2b0567ac6f
João Matos <joaocarlosm00@gmail.com>
2022-11-22 14:04:01 -0500

    TMLE and LTMLE results with merged cohorts

--- src/r_scripts/utils/rebuild_data.R
+++ src/r_scripts/utils/rebuild_data.R
@@ -4,6 +4,7 @@ rebuild_data <- function(sepsis_data, treatment) {
     # treatment should be "ventilation_bin", "rrt", or "pressor"
     # the order of the variables must be specifically this
     sepsis_data_new <- sepsis_data[, c("ethnicity_white", treatment)]
+    sepsis_data_new$source1 <- sepsis_data[, c("source")]
     sepsis_data_new$gender1 <- sepsis_data[, c("gender")]
     sepsis_data_new$anchor_age1 <- sepsis_data[, c("anchor_age")]
     sepsis_data_new$charlson_comorbidity_index1 <- sepsis_data[, c("charlson_comorbidity_index")]
@@ -21,6 +22,9 @@ rebuild_data <- function(sepsis_data, treatment) {
     }
 
     sepsis_data_new$anchor_year_group1  <- sepsis_data[, c("anchor_year_group")]
+
+
+    sepsis_data_new$source2 <- sepsis_data[, c("source")]
     sepsis_data_new$gender2 <- sepsis_data[, c("gender")]
     sepsis_data_new$anchor_age2 <- sepsis_data[, c("anchor_age")]
     sepsis_data_new$charlson_comorbidity_index2 <- sepsis_data[, c("charlson_comorbidity_index")]
@@ -48,7 +52,7 @@ rebuild_data <- function(sepsis_data, treatment) {
 data_between_sofa <- function(sepsis_data, sofa_low_inclusive, sofa_high_inclusive) {
 
     res <- sepsis_data[sepsis_data$SOFA <= sofa_high_inclusive & sepsis_data$SOFA >= sofa_low_inclusive,
-        c("anchor_age","gender","ethnicity_white","SOFA","charlson_comorbidity_index","anchor_year_group", "ventilation_bin", "death_bin", "rrt", "pressor")]
+        c("source","anchor_age","gender","ethnicity_white","SOFA","charlson_comorbidity_index","anchor_year_group", "ventilation_bin", "death_bin", "rrt", "pressor")]
     
     return(na.omit(res))
 }
@@ -57,6 +61,6 @@ data_between_sofa <- function(sepsis_data, sofa_low_inclusive, sofa_high_inclusi
 data_between_sofa_and_anchor_year_group <- function(sepsis_data, sofa_low_inclusive, sofa_high_inclusive,ayg_value) {
     
     res <- sepsis_data[sepsis_data$SOFA <= sofa_high_inclusive & sepsis_data$SOFA >= sofa_low_inclusive & sepsis_data$anchor_year_group == ayg_value,
-                       c("anchor_age","gender","ethnicity_white","SOFA","charlson_comorbidity_index", "ventilation_bin", "death_bin", "rrt", "pressor")]
+                       c("source","anchor_age","gender","ethnicity_white","SOFA","charlson_comorbidity_index", "ventilation_bin", "death_bin", "rrt", "pressor")]
     return(na.omit(res))
 }
