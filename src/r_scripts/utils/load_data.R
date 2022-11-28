e7c7ae92779656ca4631a36afaa34917f6a25620
Tristan <mirko84@yahoo.de>
2022-11-28 12:07:46 -0500

    various edits to load data

--- src/r_scripts/utils/load_data.R
+++ src/r_scripts/utils/load_data.R
@@ -18,22 +18,23 @@ load_data <- function(cohort){
   if (file_path == "data/MIMIC_data.csv") {
 
     sepsis_data <- sepsis_data %>% mutate(gender = ifelse(gender == "F", 1, 0))
-
+    
     sepsis_data <- sepsis_data %>% mutate(ventilation_bin = ifelse(InvasiveVent_hr > 0 & !is.na(InvasiveVent_hr), 1, 0))
     sepsis_data <- sepsis_data %>% mutate(pressor = ifelse(pressor=="True", 1, 0))
     sepsis_data <- sepsis_data %>% mutate(rrt = ifelse(is.na(rrt), 0, 1))
     sepsis_data <- sepsis_data %>% mutate(death_bin = ifelse(discharge_location == "DIED" | discharge_location == "HOSPICE" | dod != "", 1, 0))
     sepsis_data <- sepsis_data %>% mutate(ethnicity_white = ifelse(race == "WHITE" | race == "WHITE - BRAZILIAN" | race == "WHITE - EASTERN EUROPEAN" | race == "WHITE - OTHER EUROPEAN" | race == "WHITE - RUSSIAN" | race == "PORTUGUESE", 1, 0))
+    sepsis_data$charlson_cont <- sepsis_data$charlson_comorbidity_index # create unified and continous Charlson column
     sepsis_data <- sepsis_data %>% mutate(charlson_comorbidity_index = ifelse(
       charlson_comorbidity_index >= 0 & charlson_comorbidity_index <= 5, "0 - 5", ifelse(
         charlson_comorbidity_index >= 6 & charlson_comorbidity_index <= 10, "6 - 10", ifelse(
           charlson_comorbidity_index >= 11 & charlson_comorbidity_index <= 15, "11 - 15", "16 and above"))))
           
-  sepsis_data$los_mimic = as.numeric(difftime(sespis_data$dischtime, sespis_data$admittime, units = 'days')) # Length of stay MIMIC
-  sepsis_data$los_mimic[sepsis_data$los_mimic < 0] <- 0 # clean data to have minimum of 0 days
+    sepsis_data$los = as.numeric(difftime(sepsis_data$dischtime, sepsis_data$admittime, units = 'days')) # Length of stay MIMIC
+    sepsis_data$los[sepsis_data$los < 0] <- 0 # clean data to have minimum of 0 days
 
   } else if (file_path == "data/eICU_data.csv") {
-
+    
     sepsis_data <- sepsis_data %>% mutate(gender = ifelse(gender == "Female", 1, 0))
 
     sepsis_data <- sepsis_data %>% mutate(ventilation_bin = ifelse(is.na(VENT_final), 0, 1))
@@ -41,6 +42,7 @@ load_data <- function(cohort){
     sepsis_data <- sepsis_data %>% mutate(rrt = ifelse(is.na(RRT_final), 0, 1))
     sepsis_data <- sepsis_data %>% mutate(death_bin = ifelse(unitdischargelocation == "Death" | unitdischargestatus == "Expired" | hospitaldischargestatus == "Expired", 1, 0))
     sepsis_data <- sepsis_data %>% mutate(ethnicity_white = ifelse(race == "Caucasian", 1, 0))
+    sepsis_data$charlson_cont <- sepsis_data$charlson_comorbidity_index # create unified and continous Charlson column
     sepsis_data <- sepsis_data %>% mutate(charlson_comorbidity_index = ifelse(
       charlson_comorbidity_index >= 0 & charlson_comorbidity_index <= 5, "0 - 5", ifelse(
         charlson_comorbidity_index >= 6 & charlson_comorbidity_index <= 10, "6 - 10", ifelse(
@@ -49,15 +51,15 @@ load_data <- function(cohort){
     sepsis_data <- sepsis_data %>% mutate(anchor_age = ifelse(anchor_age == "> 89", 91, strtoi(anchor_age)))
     sepsis_data <- sepsis_data %>% mutate(anchor_year_group = as.character(anchor_year_group))
     
-    sepsis_data$los_eicu <- (sepsis_data$hospitaldischargeoffset/1440) # Generate eICU Lenght of stay
+    sepsis_data$los <- (sepsis_data$hospitaldischargeoffset/1440) # Generate eICU Lenght of stay
 
   } else {
     print("Wrong path or file name.")
   }
 
   # Return just keeping columns of interest
-  return(sepsis_data[, c("gender", "los_mimic", "los_eicu", "ventilation_bin", "pressor", "rrt", "death_bin", "ethnicity_white",
-                         "charlson_comorbidity_index", "anchor_age", "SOFA", "anchor_year_group")])
+  return(sepsis_data[, c("gender", "los", "ventilation_bin", "pressor", "rrt", "death_bin", "ethnicity_white",
+                         "charlson_cont", "charlson_comorbidity_index", "anchor_age", "SOFA", "anchor_year_group")])
 }
 
 get_merged_datasets <- function() {
