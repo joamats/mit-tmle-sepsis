fdfb4cf9d2e64030e5ef69202634ee846848b00a
Tristan <mirko84@yahoo.de>
2022-12-20 10:56:25 -0500

    changes to log reg to homog. with TMLE

--- src/r_scripts/log_reg/create_csv.r
+++
@@ -1,9 +0,0 @@
-source("src/r_scripts/utils/load_data.R")
-
-# Save MIMIC data for Stata
-sepsis_data <- load_data('MIMIC')
-write.csv(sepsis_data, "data/MIMIC_data_stata.csv")
-
-# Save eICU data for Stata
-sepsis_data <- load_data('eICU')
-write.csv(sepsis_data, "data/eICU_data_stata.csv")
--- src/r_scripts/log_reg/log_reg.r
+++ src/r_scripts/log_reg/log_reg.r
@@ -9,23 +9,14 @@ df = read_csv('data/MIMIC_eICU.csv')
 df2 = df
 
 ##########SOFA############
-df2$SOFA_new = df2$SOFA
-df2$SOFA_new[df2$SOFA >= 0 
+df2$SOFA_cat = df2$SOFA
+df2$SOFA_cat[df2$SOFA >= 0 
              & df2$SOFA <= 3] <- "0 - 3"
-df2$SOFA_new[df2$SOFA >= 4 
+df2$SOFA_cat[df2$SOFA >= 4 
              & df2$SOFA <= 6] <- "4 - 6"
-df2$SOFA_new[df2$SOFA >= 7 
+df2$SOFA_cat[df2$SOFA >= 7 
              & df2$SOFA <= 10] <- "7 - 10"
-df2$SOFA_new[df2$SOFA >= 11] <- "11 and above"
-##########Charlson############
-df2$charlson_new = df2$charlson_cont
-df2$charlson_new[df2$charlson_cont >= 0 
-                 & df2$charlson_cont <= 3] <- "0 - 3"
-df2$charlson_new[df2$charlson_cont >= 4 
-                 & df2$charlson_cont <= 6] <- "4 - 6"
-df2$charlson_new[df2$charlson_cont >= 7 
-                 & df2$charlson_cont <= 10] <- "7 - 10"
-df2$charlson_new[df2$charlson_cont >= 11] <- "11 and above"
+df2$SOFA_cat[df2$SOFA >= 11] <- "11 and above"
 
 ##########OASIS############
 df2$OASIS_cat = df2$OASIS_B
@@ -37,15 +28,9 @@ df2$OASIS_cat[df2$OASIS_B >= 46
               & df2$OASIS_B <= 51] <- "46 - 51"
 df2$OASIS_cat[df2$OASIS_B >= 52] <- "52 and above"
 
-# Assign eICU years to MIMIC year bins
-df2$anchor_year_group[df2$anchor_year_group == "2014"] <- "2014 - 2016" 
-df2$anchor_year_group[df2$anchor_year_group == "2015"] <- "2014 - 2016" 
-
 # Define factor variables
-df2$SOFA_new <- factor(df2$SOFA_new)
-df2$charlson_new <- factor(df2$charlson_new)
+df2$SOFA_cat <- factor(df2$SOFA_cat)
 df2$OASIS_cat <- factor(df2$OASIS_cat)
-df2$anchor_year_group <- factor(df2$anchor_year_group)
 df2$hypertension <- factor(df2$hypertension)
 df2$heart_failure <- factor(df2$heart_failure)
 df2$copd <- factor(df2$copd)
@@ -53,14 +38,14 @@ df2$asthma <- factor(df2$asthma)
 df2$ckd <- factor(df2$ckd)
 
 #Get all column names to run regression on
-SOFA_list = levels(df2$SOFA_new)
+SOFA_list = levels(df2$SOFA_cat)
 OASIS_list = levels(df2$OASIS_cat)
 
 ### Ventilation
 
 # Regression for SOFA all separately -> Ventilation
 mylogit <- glm(ventilation_bin ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-                 gender + anchor_year_group + SOFA + charlson_comorbidity_index + rrt + pressor, 
+                 gender + source + SOFA + charlson_cont + rrt + pressor, 
                data = df2, family=binomial(link='logit'))
 
 # summary(mylogit) // for a glimpse at the results
@@ -75,8 +60,8 @@ res_store$outcome <- "Ventilation"
 #Loop over them and create model for each
 log_models = lapply(SOFA_list, function(x){
   glm(ventilation_bin ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-        gender + anchor_year_group + SOFA + charlson_comorbidity_index + rrt + pressor,
-      data = subset(df2, SOFA_new == x), na.action = na.omit, family=binomial(link='logit'))
+        gender + source + SOFA + charlson_cont + rrt + pressor,
+      data = subset(df2, SOFA_cat == x), na.action = na.omit, family=binomial(link='logit'))
   
 })
 
@@ -97,7 +82,7 @@ for (i in 1:length(SOFA_list)) {
 
 # Regression for SOFA all separately -> RRT
 mylogit <- glm(rrt ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-                 gender + anchor_year_group + SOFA + charlson_comorbidity_index + ventilation_bin + pressor, 
+                 gender + source + SOFA + charlson_cont + ventilation_bin + pressor, 
                data = df2, family=binomial(link='logit'))
 
 res_store_new <- as.data.frame(exp(cbind(OR = coef(mylogit), confint(mylogit))) )
@@ -111,8 +96,8 @@ res_store <- rbind(res_store, res_store_new)
 # Loop over them and create model for each
 log_models = lapply(SOFA_list, function(x){
   glm(rrt ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-        gender + anchor_year_group + SOFA + charlson_comorbidity_index + ventilation_bin + pressor,
-      data = subset(df2, SOFA_new == x) ,na.action = na.omit, family=binomial(link='logit'))
+        gender + source + SOFA + charlson_cont + ventilation_bin + pressor,
+      data = subset(df2, SOFA_cat == x) ,na.action = na.omit, family=binomial(link='logit'))
   
 })
 
@@ -133,7 +118,7 @@ for (i in 1:length(SOFA_list)) {
 
 # Regression for SOFA all separately -> Vasopressors
 mylogit <- glm(pressor ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-                 gender + anchor_year_group + SOFA + charlson_comorbidity_index + ventilation_bin + rrt, 
+                 gender + source + SOFA + charlson_cont + ventilation_bin + rrt, 
                data = df2, family=binomial(link='logit'))
 
 res_store_new <- as.data.frame(exp(cbind(OR = coef(mylogit), confint(mylogit))) )
@@ -147,8 +132,8 @@ res_store <- rbind(res_store, res_store_new)
 # Loop over them and create model for each
 log_models = lapply(SOFA_list, function(x){
   glm(pressor ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-        gender + anchor_year_group + SOFA + charlson_comorbidity_index + ventilation_bin + rrt,
-      data = subset(df2, SOFA_new == x), na.action = na.omit, family=binomial(link='logit'))
+        gender + source + SOFA + charlson_cont + ventilation_bin + rrt,
+      data = subset(df2, SOFA_cat == x), na.action = na.omit, family=binomial(link='logit'))
   
 })
 
@@ -171,13 +156,16 @@ res_store$SOFA_start[res_store$model == "2"] <- 4
 res_store$SOFA_start[res_store$model == "3"] <- 7
 res_store$SOFA_start[res_store$model == "4"] <- 11
 
+write.csv(res_store, 
+          'results/logit_res_SOFA.csv',
+          row.names=TRUE)
 
 ################ OASIS ###############  
 ### Ventilation
 
 # Regression for OASIS all separately -> Ventilation
 mylogit <- glm(ventilation_bin ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-                 gender + anchor_year_group + OASIS_B + charlson_new + rrt + pressor, 
+                 gender + source + OASIS_B + charlson_cont + rrt + pressor, 
                data = df2, family=binomial(link='logit'))
 
 # summary(mylogit) // for a glimpse at the results
@@ -191,7 +179,7 @@ res_store$outcome <- "Ventilation"
 # Loop over them and create model for each
 log_models = lapply(OASIS_list, function(x){
   glm(ventilation_bin ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-        gender + anchor_year_group + OASIS_B + charlson_new + rrt + pressor,
+        gender + source + OASIS_B + charlson_cont + rrt + pressor,
       data = subset(df2, OASIS_cat == x), na.action = na.omit, family=binomial(link='logit'))
 })
 
@@ -211,7 +199,7 @@ for (i in 1:length(OASIS_list)) {
 
 # Regression for OASIS all separately -> RRT
 mylogit <- glm(rrt ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-                 gender + anchor_year_group + OASIS_B + charlson_new + ventilation_bin + pressor, 
+                 gender + source + OASIS_B + charlson_cont + ventilation_bin + pressor, 
                data = df2, family=binomial(link='logit'))
 
 res_store_new <- as.data.frame(exp(cbind(OR = coef(mylogit), confint(mylogit))) )
@@ -225,7 +213,7 @@ res_store <- rbind(res_store, res_store_new)
 # Loop over them and create model for each
 log_models = lapply(OASIS_list, function(x){
   glm(rrt ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-        gender + anchor_year_group + OASIS_B + charlson_new + ventilation_bin + pressor,
+        gender + source + OASIS_B + charlson_cont + ventilation_bin + pressor,
       data = subset(df2, OASIS_cat == x) ,na.action = na.omit, family=binomial(link='logit'))
 })
 
@@ -246,7 +234,7 @@ for (i in 1:length(OASIS_list)) {
 
 # Regression for SOFA all separately -> Vasopressors
 mylogit <- glm(pressor ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-                 gender + anchor_year_group + OASIS_B + charlson_new + ventilation_bin + rrt, 
+                 gender + source + OASIS_B + charlson_cont + ventilation_bin + rrt, 
                data = df2, family=binomial(link='logit'))
 
 res_store_new <- as.data.frame(exp(cbind(OR = coef(mylogit), confint(mylogit))) )
@@ -260,7 +248,7 @@ res_store <- rbind(res_store, res_store_new)
 # Loop over them and create model for each
 log_models = lapply(OASIS_list, function(x){
   glm(pressor ~ ethnicity_white + anchor_age + hypertension + heart_failure + copd + asthma + ckd +
-        gender + anchor_year_group + OASIS_B + charlson_new + ventilation_bin + rrt,
+        gender + source + OASIS_B + charlson_cont + ventilation_bin + rrt,
       data = subset(df2, OASIS_cat == x), na.action = na.omit, family=binomial(link='logit'))
 })
 
@@ -284,7 +272,7 @@ res_store$OASIS_start[res_store$model == "3"] <- 48
 res_store$OASIS_start[res_store$model == "4"] <- 52
 
 write.csv(res_store, 
-          'results/logit_res.csv',
+          'results/logit_res_OASIS.csv',
           row.names=TRUE)
 
 
