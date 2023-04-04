4f881c267a850d5419fc83b958eacbc09413bbbc
João Matos <joaocarlosm00@gmail.com>
2023-04-04 18:07:07 +0100

    Read confounders, tratments and ranges from txt

---
+++ src/r_scripts/tmle_NEW.R
@@ -0,0 +1,111 @@
+source("src/r_scripts/load_data.R")
+source("src/r_scripts/utils.R")
+
+# run TMLE 
+run_tmle <- function(data, treatment, confounders, outcome,
+                     cohort, race, sev_min, sev_max, results_df) {
+
+
+    W <- data[, confounders]
+    A <- data[, treatment]
+    Y <- data[, outcome]
+
+    result <- tmle(
+                Y = Y,
+                A = A,
+                W = W,
+                family = "binomial", 
+                gbound = c(0.05, 0.95),
+                g.SL.library = c("SL.glm"),
+                Q.SL.library = c("SL.glm"),
+                )
+
+    log <- summary(result)     
+
+    results_df[nrow(results_df) + 1,] <- c(
+                                            treatment,
+                                            cohort,
+                                            race,
+                                            sev_min,
+                                            sev_max,
+                                            log$estimates$ATE$psi,
+                                            log$estimates$ATE$CI[1],
+                                            log$estimates$ATE$CI[2],
+                                            log$estimates$ATE$pvalue,
+                                            nrow(data)
+                                            ) 
+    return (results_df)
+}
+
+# Main
+cohorts <- c("MIMIC")
+races <- c("all", "white", "non-white")
+prob_mort_ranges <- read.csv("config/prob_mort_ranges.csv")
+treatments <- read.delim("config/treatments.txt")
+confounders <- read.delim("config/confounders.txt")
+outcome <- "mortality_in"
+
+# Dataframe to hold results
+results_df <- data.frame(matrix(ncol=10, nrow=0))
+colnames(results_df) <- c(
+                          "treatment",
+                          "cohort",
+                          "race",
+                          "prob_mort_start",
+                          "prob_mort_end",
+                          "psi",
+                          "i_ci",
+                          "s_ci",
+                          "pvalue",
+                          "n")
+
+
+for (c in cohorts) {
+    print(paste0("Cohort: ", c))
+
+    # Read Data for this database and cohort
+    data <- read.csv(paste0("data/", c, ".csv"))
+
+    for (j in 1:nrow(treatments)) {
+        # Treatment
+        treatment <- treatments$treatment[j]
+        print(paste0("Treatment: ", treatment))
+
+        # Get formula with confounders and treatment
+        model_confounders <- read_confounders(j, treatments, confounders) 
+
+        for (r in races) {
+
+            print(paste0("Race: ", r))
+
+            if (race == "non-white") {
+                subset_data <- subset(data, ethnicity_white == 0)
+                
+            } else if (race == "white") {
+                subset_data <- subset(data, ethnicity_white == 1)
+                
+            } # else, nothing because race = "all" needs no further filtering
+
+            for (i in 1:nrow(prob_mort_ranges)) {
+                
+                sev_min <- prob_mort_ranges$min[i]
+                sev_max <- prob_mort_ranges$max[i]
+
+                print(paste0("Stratification by prob_mort: ", sev_min, " - ", sev_max))
+
+                # Stratify by prob_mort
+                subsubset_data <- subset(data, prob_mort >= sev_min & prob_mort < sev_max)
+
+                # Run TMLE
+                results_df <- run_tmle(subsubset_data, treatment, model_confounders, outcome,
+                                       c, race, sev_min, sev_max, results_df)
+
+                # Save Results
+                write.csv(results_df, paste0("results/NEW/TMLE_", c,".csv"))
+
+            }
+        }           
+    }
+}
+
+
