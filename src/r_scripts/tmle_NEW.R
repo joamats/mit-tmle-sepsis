02616dfbda36a30ea4f8b701998bb4445f6a1499
João Matos <joaocarlosm00@gmail.com>
2023-04-11 16:33:00 +0100

    Insulin as control; SL libraries; No Race strata in control

--- src/r_scripts/tmle_NEW.R
+++ src/r_scripts/tmle_NEW.R
@@ -2,7 +2,7 @@ source("src/r_scripts/load_data.R")
 source("src/r_scripts/utils.R")
 
 # run TMLE 
-run_tmle <- function(data, treatment, confounders, outcome,
+run_tmle <- function(data, treatment, confounders, outcome, SL_libraries,
                      cohort, race, sev_min, sev_max, results_df) {
 
 
@@ -16,13 +16,13 @@ run_tmle <- function(data, treatment, confounders, outcome,
                 W = W,
                 family = "binomial", 
                 gbound = c(0.05, 0.95),
-                g.SL.library = c("SL.glm"),
-                Q.SL.library = c("SL.glm"),
+                g.SL.library = SL_libraries$SL_library,
+                Q.SL.library = SL_libraries$SL_library
                 )
 
-    log <- summary(result)     
+    log <- summary(result)   
 
-    results_df[nrow(results_df) + 1,] <- c(
+    results_df[nrow(results_df) + 1,] <- c( outcome,
                                             treatment,
                                             cohort,
                                             race,
@@ -32,17 +32,20 @@ run_tmle <- function(data, treatment, confounders, outcome,
                                             log$estimates$ATE$CI[1],
                                             log$estimates$ATE$CI[2],
                                             log$estimates$ATE$pvalue,
-                                            nrow(data)
+                                            nrow(data),
+                                            paste(SL_libraries$SL_library, collapse = " "),
+                                            paste(result$Qinit$coef, collapse = " "),
+                                            paste(result$g$coef, collapse = " ")
                                             ) 
     return (results_df)
 }
 
 # Main
 cohorts <- c("MIMIC")
-races <- c("white", "non-white") 
-outcomes <- c("blood_yes", "mortality_in")
+outcomes <- c("insulin_yes", "blood_yes", "mortality_in")
 prob_mort_ranges <- read.csv("config/prob_mort_ranges.csv")
 treatments <- read.delim("config/treatments.txt")
+SL_libraries <- read.delim("config/SL_libraries.txt")
 
 
 for (c in cohorts) {
@@ -57,8 +60,9 @@ for (c in cohorts) {
         print(paste0("Outcome: ", outcome))
 
         # Dataframe to hold results
-        results_df <- data.frame(matrix(ncol=10, nrow=0))
+        results_df <- data.frame(matrix(ncol=14, nrow=0))
         colnames(results_df) <- c(
+                                "outcome",
                                 "treatment",
                                 "cohort",
                                 "race",
@@ -68,7 +72,16 @@ for (c in cohorts) {
                                 "i_ci",
                                 "s_ci",
                                 "pvalue",
-                                "n")
+                                "n",
+                                "SL_libraries",
+                                "Q_weights",
+                                "g_weights")
+
+        if (outcome == "mortality_in") {
+            races <- c("white", "non-white") 
+        } else {
+            races <- c("all") 
+        }
 
         for (j in 1:nrow(treatments)) {
             # Treatment
@@ -88,7 +101,9 @@ for (c in cohorts) {
                 } else if (r == "white") {
                     subset_data <- subset(data, ethnicity_white == 1)
                     
-                } # else, nothing because race = "all" needs no further filtering
+                } else {
+                    subset_data <- data
+                }
 
                 for (i in 1:nrow(prob_mort_ranges)) {
                     
@@ -101,8 +116,8 @@ for (c in cohorts) {
                     subsubset_data <- subset(subset_data, prob_mort >= sev_min & prob_mort < sev_max)
 
                     # Run TMLE
-                    results_df <- run_tmle(subsubset_data, treatment, model_confounders, outcome,
-                                        c, r, sev_min, sev_max, results_df)
+                    results_df <- run_tmle(subsubset_data, treatment, model_confounders, outcome, 
+                                           SL_libraries, c, r, sev_min, sev_max, results_df)
 
                     # Save Results
                     write.csv(results_df, paste0("results/NEW/", c, "/", outcome, ".csv"))
